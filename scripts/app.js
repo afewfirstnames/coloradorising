bfdfa9981db75b4c7f726dea6423794b56f2faa1
afewfirstnames <loganmtyler@gmail.com>
2021-03-12 14:55:33 -0700

    video tweak

--- scripts/app.js
+++ scripts/app.js
@@ -45,6 +45,33 @@ window.addEventListener("resize", function () {
   }
 });
 
+$(document).ready(function () {
+  $(".news-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    arrows: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
 // WHAT IS FRACKING ANIMATION */
 
 // // using d3 for convenience
